4d61ffd23d4ff6da477253405e12ed86792f0e79
Mike Smith <mikesmith7810@gmail.com>
2021-12-06 14:57:02 +0000

    intial checkin

---
+++ src/main/java/com/guardian/whoscallingme/WhoscallingmeApplication.java
@@ -0,0 +1,13 @@
+package com.guardian.whoscallingme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhoscallingmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhoscallingmeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guardian/whoscallingme/WhoscallingmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guardian.whoscallingme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhoscallingmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
